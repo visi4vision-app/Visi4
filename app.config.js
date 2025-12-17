4999c6de2cafccda77346e72b1ab6e011640e18b
freemindvision454 <freemindvision454@gmail.com>
2025-12-17 06:29:31 +0000

    lock logo: force custom app icon

--- app.config.js
+++ app.config.js
@@ -2,26 +2,22 @@ export default {
   expo: {
     name: "Visi4",
     slug: "visi4",
+    version: "1.0.0",
     icon: "./assets/icon.png",
-
     splash: {
-      image: "./assets/icon.png",
+      image: "./assets/splash.png",
       resizeMode: "contain",
       backgroundColor: "#000000"
     },
-
     android: {
       package: "com.freemindvision.visi4",
       adaptiveIcon: {
-        foregroundImage: "./assets/icon.png",
+        foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#000000"
       }
     },
-
-    web: {
-      favicon: "./assets/icon.png"
-    },
-
-    plugins: ["expo-router"]
+    ios: {
+      bundleIdentifier: "com.freemindvision.visi4"
+    }
   }
-};
+}
