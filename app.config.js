91a3aecf7d946ce3f8e451cf5e248c849ab68374
freemindvision454 <freemindvision454@gmail.com>
2025-12-17 08:11:09 +0000

    update

--- app.config.js
+++ app.config.js
@@ -3,12 +3,19 @@ export default {
     name: "Visi4",
     slug: "visi4",
     version: "1.0.0",
+
+    assetBundlePatterns: [
+      "assets/*"
+    ],
+
     icon: "./assets/icon.png",
+
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
       backgroundColor: "#000000"
     },
+
     android: {
       package: "com.freemindvision.visi4",
       adaptiveIcon: {
