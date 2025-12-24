a595cd3f09f8011d1e09fa8d7059438aa786a0b0
FreeMind Vision <freemindvision454@gmail.com>
2025-12-24 19:30:44 +0000

    fix: metro config compatible with Node 18 (EAS)

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,10 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Compatibilité Node 18 (EAS)
+if (Array.isArray(config.resolver.assetExts)) {
+  config.resolver.assetExts = [...config.resolver.assetExts].reverse();
+}
 
 module.exports = config;
