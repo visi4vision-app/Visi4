606600d8f76be4bb280c987016240e672ca4cae9
FreeMind Vision <freemindvision454@gmail.com>
2025-12-24 19:58:58 +0000

    fix: Node 18 compatibility (remove toReversed)

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Node 18 compatible (PAS de toReversed)
+// ✅ Node 18 compatible (PAS de toReversed)
 if (Array.isArray(config.resolver.assetExts)) {
-  config.resolver.assetExts = .slice().reverse()...config.resolver.assetExts].reverse();
+  config.resolver.assetExts = [...config.resolver.assetExts].reverse();
 }
 
 module.exports = config;
