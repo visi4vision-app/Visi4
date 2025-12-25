4e09d2433db1ae7b48add791959aa759c3a88a9f
FreeMind Vision <freemindvision454@gmail.com>
2025-12-25 18:28:45 +0000

    fix: properly export eas projectId in app.config.js

--- app.config.js
+++ app.config.js
@@ -1,32 +1,20 @@
-export default {
-  expo: {
-    owner: "freemindvision_app",
+export default ({ config }) => ({
+  ...config,
+  name: "Visi4",
+  slug: "visi4",
+  owner: "freemindvision_app",
 
-    name: "Visi4",
-    slug: "visi4",
-    version: "1.0.4",
-    sdkVersion: "51.0.0",
+  android: {
+    package: "com.freemindvision.visi4"
+  },
 
-    icon: "./assets/logo.png",
+  ios: {
+    bundleIdentifier: "com.freemindvision.visi4"
+  },
 
-    assetBundlePatterns: ["assets/*"],
-
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#000000"
-    },
-
-    android: {
-      package: "com.freemindvision.visi4",
-      adaptiveIcon: {
-        foregroundImage: "./assets/logo.png",
-        backgroundColor: "#000000"
-      }
-    },
-
-    ios: {
-      bundleIdentifier: "com.freemindvision.visi4"
+  extra: {
+    eas: {
+      projectId: "48700c55-8bd7-49c7-86d9-9a589f3a5c05"
     }
   }
-};
+});
