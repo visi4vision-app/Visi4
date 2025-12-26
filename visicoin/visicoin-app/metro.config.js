cac7621d5988a59b0e4b7f7b6318c714847e5f32
FreeMind Vision <freemindvision454@gmail.com>
2025-12-26 12:55:31 +0000

    chore: remove nested Expo app to fix EAS build

--- visicoin/visicoin-app/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.watchFolders = [];
-config.resolver.nodeModulesPaths = [__dirname + "/node_modules"];
-
-module.exports = config;
